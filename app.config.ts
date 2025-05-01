8ea96a2a0bb61e49c8488518bbda389dcc8c243d
Alexis LUCAS <sixela.sacul.pro@protonmail.com>
2025-05-01 23:26:49 +0200

    final fix (I hope)

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from '@tanstack/react-start/config'
 import { cloudflare } from 'unenv'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
+// may be cleaner to use the deny imports from vite-env-only, especially since
+// i'm already splitting the lib folders into client, server, and iso
+// https://www.npmjs.com/package/vite-env-only
+
 export default defineConfig({
   // https://github.com/TanStack/router/discussions/2863?sort=new#discussioncomment-12458714
   tsr: {
